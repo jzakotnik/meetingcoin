a7ef7f6dfceb658d563e9462b0c10fe799942b1a
Jure Zakotnik <jzakotnik@macbook.fritz.box>
2019-05-18 23:07:04 +0200

    added coin

--- contracts/MeetingCoin.sol
+++ contracts/MeetingCoin.sol
@@ -1,8 +1,12 @@
-pragma solidity 0.5.0;
+pragma solidity 0.5.2;
 
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";  
+
 
 contract MeetingCoin is ERC20Detailed {
+    constructor(string memory _name, string memory _symbol, uint8 _decimals)
+    ERC20Detailed(_name, _symbol, _decimals)
+    public {
 
-
+    }
 }
